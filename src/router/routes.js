b11b8a8e53bc59ee7e30712e171c13656bedbc58
Luke Luo 罗恩东 <luke.luo@leapting.com>
2022-12-06 10:05:30 +0800

    router

---
+++ src/router/routes.js
@@ -0,0 +1,20 @@
+export default [
+  // 重定向
+  {
+    path: '/',
+    redirect: '/home',
+    meta: { isShow: true },
+  },
+  {
+    path: '/home',
+    component: () => import('@/pages/Home'),
+    meta: { isShow: true },
+  },
+  {
+    path: '/search/:keyword?',
+    component: () => import('@/pages/Search'),
+    name: 'search',
+    meta: { isShow: true, title: '搜索' },
+  },
+  
+];
